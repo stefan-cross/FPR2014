7148397631db7a9c47d06d58af6f8a655acbacf7
stefancross <stefan.cross@o2.com>
2014-04-06 21:07:50 +0100

    Testing.hs now combines all Q13 and Q14, nearly there

--- Q14.1_prog.hs
+++ Q14.1_prog.hs
@@ -7,7 +7,7 @@ type Layout = [Layer]
 type Layer = [Connection] 
 type Connection = (Size, Size)
 
-type MyString = Char
+type String' = Char
 
 data Circuit
     = Id Size
@@ -17,30 +17,46 @@ data Circuit
     | Above Circuit Circuit
     deriving(Show)
 
-header = " <svg width='$20' height='$20' viewBox='-10,-10,$20,$20' xmlns='http://www.w3.org/2000/svg' version='1.1'> \n"
-line = " <line x1='$00' y1='0' x2='$00' y2='£00' stroke='black' stroke-width='2'/> \n"
+-- SVG constructors
+header = "<svg width='$20' height='$20' viewBox='-10,-10,$20,$20' xmlns='http://www.w3.org/2000/svg' version='1.1'> \n"
+line = "<line x1='$00' y1='0' x2='$00' y2='£00' " ++ style ++ "/> \n"
+fan = "<line x1='α00' y1='β00' x2='ɣ00' y2='ƍ00' " ++ style ++ "/> \n" -- hack with Greek symbols
 point = "<circle cx='$00' cy='£00' r='7' fill='black' stroke-width='0'/>\n"
-footer = " </svg> "
+style = " stroke='black' stroke-width='2' "
+footer = "</svg> "
 
 replace _ _ [] = []
 replace a b (x:xs)
     | x == a = b:replace a b xs
     | otherwise = x:replace a b xs
 
+createHeader s = (replace '$' (intToDigit s) header) 
+
 createLine i c 
-    | i >= 0 = (replace '$' (intToDigit i) (replace '£' (intToDigit c) line)) : createLine (des i) c
+    | i >= 0 = (replace '$' (intToDigit i) (replace '£' (intToDigit c) line)) : createLine (i-1) c
     | otherwise = []
-    where 
-    	des i = i - 1 
 
-createPoint (xs:xss) = (replace '$' (intToDigit(fst(head xs))) (replace '£' (intToDigit(snd(head xs))) point)) : createPoint xss
-createPoint [] = []
+createPoint (xs:xss) i = if length xs > 1 
+	then point' xs i : point'' xs i  : createPoint ((tail xs):xss) i
+		else point' xs i : point'' xs i  : createPoint xss (i + 1)
+createPoint [] _ = []
+-- Takes list and int, extracts fst val of tuple, replaces $ for int val, point'' works on snd val of tuple where i is layer
+point' xs i = (replace '$' (intToDigit(fst(head xs))) (replace '£' (intToDigit i) point))
+point'' xs i = (replace '$' (intToDigit(snd(head xs))) (replace '£' (intToDigit (i + 1)) point))
 
-svg :: (Layout , Size) -> [MyString]
-svg (lx, s) = concat [createHeader s, concat(createLine s s), concat(createPoint lx), createFooter]
-    where
-        createHeader s = (replace '$' (intToDigit s) header) 
-        createFooter = footer
+createFan (xs:xss) i = if length xs > 1
+	then fan' xs i : createFan ((drop 1 xs):xss) i
+		else fan' xs i : createFan xss (i + 1)
+createFan [] _ = []
+-- Takes list and int and subs out greek letters for values, these are nested, working through tuple vals and layer i val
+fan' xs i = (replace 'α' (intToDigit(fst(head xs))) (replace 'β' (intToDigit i) (replace 'ɣ' (intToDigit(snd(head xs))) (replace 'ƍ' (intToDigit (i + 1)) fan))))
+
+svg :: (Layout , Size) -> [String']
+svg (lx, s) = concat [createHeader (s - 1), concat(createLine s s), concat(createPoint lx 0), concat(createFan lx 0), footer]
+        
+
+-- output :: MyString -> Circuit -> IO()
+-- output file c = writeFile file (unlines(svg(layout c, width c)))
 
 createFile (xs, s) = do
 	writeFile "example2.svg" (svg (xs, s))
--- testing.hs
+++ testing.hs
@@ -2,11 +2,14 @@
 import Q1_prog
 import Q12_prog
 import Data.List
+import System.IO
+import Data.Char
 
 type Layout = [Layer]
 type Layer = [Connection] 
-type Connection = Maybe (Size, Size)
+type Connection = (Size, Size)
 
+type String' = Char
 
 zipCir :: Circuit -> [[Circuit]]
 zipCir cir = case cir of
@@ -24,16 +27,61 @@ zipList = \xs yss -> zipList' xs yss
     	zipList' xs (ys:yss) = zip xs ys : zipList xs yss
 
 layout :: Circuit -> Layout
-layout cir = case cir of
-    (Above a b) -> (layout a) ++ (layout b)
-    (_) -> (layer cir) : []
+layout cir = pack(zipList [0..] (zipCir cir)) 
     where
-    	zipped = (zipList [0..] (zipCir cir)) 
-        w = width cir
-        layer l = case l of
-            (Beside a b) -> (layer a) ++ (layer b)
-            (_) -> (calc l 0) : [] 
-        calc a i = case a of
-            (Stretch (xs) f) -> Just ((head xs) -1 , sum(tail(xs)) + 1)
-            (Fan f) ->  Just (f, f)
-            (Id i) -> Nothing
+    	pack(xs:xss) = if length xs > 1
+    		then (package xs:[]): (pack ((tail xs):xss))
+    	    else (package xs:[]): pack xss
+    	pack(_) = []
+    	package (x:xs) = case x of
+    		(i, (Fan f)) -> (i, (i + f - 1))
+    		(i, (Stretch ys f)) -> ((head ys) -1, (sum ys)-1)
+    		(i, a) -> package xs
+
+
+-- SVG constructors
+header = "<svg width='$20' height='$20' viewBox='-10,-10,$20,$20' xmlns='http://www.w3.org/2000/svg' version='1.1'> \n"
+line = "<line x1='$00' y1='0' x2='$00' y2='£00' " ++ style ++ "/> \n"
+fan = "<line x1='α00' y1='β00' x2='ɣ00' y2='ƍ00' " ++ style ++ "/> \n" -- hack with Greek symbols
+point = "<circle cx='$00' cy='£00' r='7' fill='black' stroke-width='0'/>\n"
+style = " stroke='black' stroke-width='2' "
+footer = "</svg> "
+
+replace _ _ [] = []
+replace a b (x:xs)
+    | x == a = b:replace a b xs
+    | otherwise = x:replace a b xs
+
+createHeader s = (replace '$' (intToDigit s) header) 
+
+createLine i c 
+    | i >= 0 = (replace '$' (intToDigit i) (replace '£' (intToDigit c) line)) : createLine (i-1) c
+    | otherwise = []
+
+createPoint (xs:xss) i = if length xs > 1 
+	then point' xs i : point'' xs i  : createPoint ((tail xs):xss) i
+		else point' xs i : point'' xs i  : createPoint xss (i + 1)
+createPoint [] _ = []
+-- Takes list and int, extracts fst val of tuple, replaces $ for int val, point'' works on snd val of tuple where i is layer
+point' xs i = (replace '$' (intToDigit(fst(head xs))) (replace '£' (intToDigit i) point))
+point'' xs i = (replace '$' (intToDigit(snd(head xs))) (replace '£' (intToDigit (i + 1)) point))
+
+createFan (xs:xss) i = if length xs > 1
+	then fan' xs i : createFan ((tail xs):xss) i
+		else fan' xs i : createFan xss (i + 1)
+createFan [] _ = []
+-- Takes list and int and subs out greek letters for values, these are nested, working through tuple vals and layer i val
+fan' xs i = (replace 'α' (intToDigit(fst(head xs))) (replace 'β' (intToDigit i) (replace 'ɣ' (intToDigit(snd(head xs))) (replace 'ƍ' (intToDigit (i + 1)) fan))))
+
+svg :: (Layout , Size) -> [String']
+svg (lx, s) = concat [createHeader (s - 1), concat(createLine s s), concat(createPoint lx 0), concat(createFan lx 0), footer]
+        
+output :: FilePath -> Circuit -> IO()
+output file c = writeFile file (unlines([svg(layout c, width c)]))
+
+
+
+
+
+
+
